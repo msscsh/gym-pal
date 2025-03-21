94f3098117235f5860350bfd2465cdf5b15fcbde
Bruno Marvin <51372744+msscsh@users.noreply.github.com>
2025-03-21 18:15:11 -0300

    Update update.js

--- update.js
+++ update.js
@@ -2,7 +2,7 @@ console.log("Script carregado com sucesso!");
 
 function minhaFuncao() {
   if (confirm("Posso falar sua senha?")) {
-      alert('Sua senha é: 111711');
+      alert('Sua senha é: 111811');
   }
 }
 
